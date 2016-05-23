000ab40e0bee8a15a7415ec41a4f320d559c4653
Thiago Rider Augusto <thiago.rider@gmail.com>
2016-05-23 16:45:48 -0300

    Added 'docker-entrypoint.sh' template. Changed 'Dockerfile' to install 'gosu' utility.

---
+++ Mailserver/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if []; then
+
+fi
+
+if []; then
+	if []; then
+
+	fi
+
+	exec gosu postfix "$@"
+
+	echo
+	echo 'Postfix init process complete; ready for start up.'
+	echo
+
+fi
+
+exec "$@"
